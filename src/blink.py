c5be5fecb635511098cbff0c3c0e2f9dc333224f
matheus sonyer <matheus.sounier2024@gmail.com>
2026-06-08 16:34:38 -0400

    chore: all the libs

---
+++ src/blink.py
@@ -0,0 +1,10 @@
+import cv2
+import mediapipe as mp
+import time
+import threading
+import keyboard
+from math import dist
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from cvzone.PlotModule import LivePlot
+
