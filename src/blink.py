52e8a75dff11e40e7ff5f514106be58a3733e628
matheus sonyer <matheus.sounier2024@gmail.com>
2026-06-10 14:33:47 -0400

    chore: adds EAR function which calculates eye vertical and horizontal landmarks

--- src/blink.py
+++ src/blink.py
@@ -66,8 +66,8 @@ EYE_HORIZONTAL = (33, 133)
 BLINK_THRESHOLD     = 0.30
 PRESS_HOLD_TIME     = 0.0008
 RELEASE_HOLD_TIME   = 0.0004
-PROCESS_EVERY_N_FRAMES = 2 
-PLOT_EVERY_N_FRAMES = 4 
+PROCESS_EVERY_N_FRAMES = 2
+PLOT_EVERY_N_FRAMES = 4
 DISPLAY_WINDOW      = True
 
 blinking         = False
@@ -78,3 +78,31 @@ last_blink_score = 0.0
 last_landmarks   = None
 
 space_held = False
+
+plotY = LivePlot(640, 360, [0, 60], invert=True)
+
+## It causes delay
+def eye_aspect_ratio(vertical_pairs, horizontal_pair, landmarks, w, h):
+    """this function calculates the Eye Aspect Ratio"""
+    used_points = set()
+    for pair in vertical_pairs:
+        used_points.update(pair)
+    used_points.update(horizontal_pair)
+
+    landmark_points = {}
+    for idx in used_points:
+        lm = landmarks[idx]
+        landmark_points[idx] = (int(lm.x * w), int(lm.y * h))
+
+    vertical_sum = sum(
+        dist(landmark_points[top], landmark_points[bottom])
+        for top, bottom in vertical_pairs
+    )
+    horizontal = dist(
+        landmark_points[horizontal_pair[0]],
+        landmark_points[horizontal_pair[1]]
+    )
+
+    return (vertical_sum / len(vertical_pairs)) / horizontal, landmark_points
+
+cam = CameraStream(0)
