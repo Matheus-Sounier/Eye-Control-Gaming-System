c9efcbe4d9e146147a8765fc25b9a0ed83ef65e9
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-06-11 10:48:08 -0400

    test: test in each index, if works an index, show webcam

---
+++ tests/test_camera.py
@@ -0,0 +1,31 @@
+import cv2
+
+print(cv2.__version__)
+
+for backend_name, backend in [
+    ("CAP_ANY", cv2.CAP_ANY),
+    ("CAP_DSHOW", cv2.CAP_DSHOW),
+    ("CAP_MSMF", cv2.CAP_MSMF),
+]:
+    print(f"\n=== {backend_name} ===")
+
+    for i in range(5):
+        print(f"Testando indice {i}")
+
+        cap = cv2.VideoCapture(i, backend)
+
+        opened = cap.isOpened()
+        print("isOpened:", opened)
+
+        if opened:
+            ret, frame = cap.read()
+            print("frame:", ret)
+
+            if ret:
+                print("Success")
+                cv2.imshow("cam", frame)
+                cv2.waitKey(3000)
+                cv2.destroyAllWindows()
+                exit()
+
+        cap.release()
